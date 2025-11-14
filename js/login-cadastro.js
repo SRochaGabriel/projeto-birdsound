7b4acd9c09503587405fc11a3a3efd12443ebd85
SRochaGabriel <gabrielrocha2004.gr@gmail.com>
2025-11-14 20:59:35 -0300

    Alterações

--- js/login-cadastro.js
+++ js/login-cadastro.js
@@ -5,12 +5,11 @@ const inputTel = document.querySelector('#tel');
 
 // Lógica para esconder/exibir senha
 iconSenha.addEventListener('click', () => {
-    // Se o tipo do input for password, altera para text e troca o ícone
     if (inputSenha.type === 'password') {
         inputSenha.type = 'text';
         iconSenha.classList.remove('fa-eye');
         iconSenha.classList.add('fa-eye-slash');
-    } else { // Caso contrário, altera para password e troca o ícone
+    } else {
         inputSenha.type = 'password';
         iconSenha.classList.remove('fa-eye-slash');
         iconSenha.classList.add('fa-eye');
@@ -22,6 +21,7 @@ inputTel.addEventListener('input', () => {
     // Limpa do input qualquer coisa que não seja dígito
     inputTel.value = inputTel.value.replace(/\D/g, '');
 
+    // Transforma o formato
     inputTel.value = inputTel.value.replace(/(\d{2})(\d{5})(\d{4})/g, '($1) $2-$3');
 });
 
@@ -38,7 +38,7 @@ inputCpfCnpj.addEventListener('input', () => {
     }
 });
 
-// Definindo a função que realiza a formatação do valor de CPF
+// Realiza a formatação do valor de CPF
 function mascaraCPF(valor) {
     // Usa a função replace capturando grupos do valor com uma regular expression e depois retorna no formato 'xxx.xxx.xxx-xx'
     return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
